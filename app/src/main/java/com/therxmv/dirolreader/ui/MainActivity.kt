aff92b08273687e38766b95bbd33a3df2969f548
therxmv <roma.kyzmuch@gmail.com>
2024-06-07 18:06:57 +0300

    Removed NavHost

--- app/src/main/java/com/therxmv/dirolreader/ui/MainActivity.kt
+++ app/src/main/java/com/therxmv/dirolreader/ui/MainActivity.kt
@@ -3,10 +3,20 @@ package com.therxmv.dirolreader.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import com.therxmv.dirolreader.ui.navigation.DirolNavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.therxmv.dirolreader.ui.auth.view.AuthScreen
+import com.therxmv.dirolreader.ui.navigation.Destination
+import com.therxmv.dirolreader.ui.news.NewsScreen
+import com.therxmv.dirolreader.ui.profile.ProfileScreen
+import com.therxmv.dirolreader.ui.settings.StorageScreen
+import com.therxmv.dirolreader.ui.settings.ThemingScreen
 import com.therxmv.dirolreader.ui.theme.AppTheme
+import com.therxmv.otaupdates.presentation.view.OtaScreen
 import com.therxmv.sharedpreferences.repository.AppSharedPrefsRepository
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -20,14 +30,79 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             val isDynamic = remember { mutableStateOf(appSharedPrefsRepository.isDynamic) }
 
             AppTheme(
                 dynamicColor = isDynamic.value
             ) {
-                DirolNavHost {
-                    isDynamic.value = it
-                    appSharedPrefsRepository.isDynamic = it
+                Surface {
+                    NavHost(navController = navController, startDestination = Destination.OtaScreen.route) {
+                        composable(route = Destination.OtaScreen.route) {
+                            OtaScreen(
+                                onNavigateToAuth = {
+                                    navController.navigate(Destination.AuthScreen.route) {
+                                        popUpTo(Destination.OtaScreen.route) { inclusive = true }
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+                        }
+                        composable(route = Destination.AuthScreen.route) {
+                            AuthScreen(
+                                onNavigateToNews = {
+                                    navController.navigate(Destination.NewsScreen.route) {
+                                        popUpTo(Destination.AuthScreen.route) { inclusive = true }
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+                        }
+                        composable(route = Destination.NewsScreen.route) {
+                            NewsScreen(
+                                onNavigateToProfile = {
+                                    navController.navigate(Destination.ProfileScreen.route) {
+                                        popUpTo(Destination.NewsScreen.route) { inclusive = false }
+                                    }
+                                }
+                            )
+                        }
+                        composable(route = Destination.ProfileScreen.route) {
+                            ProfileScreen(
+                                navController = navController,
+                                onNavigateToTheming = {
+                                    navController.navigate(Destination.SettingsThemingScreen.route) {
+                                        popUpTo(Destination.ProfileScreen.route) { inclusive = false }
+                                    }
+                                },
+                                onNavigateToStorage = {
+                                    navController.navigate(Destination.SettingsStorageScreen.route) {
+                                        popUpTo(Destination.ProfileScreen.route) { inclusive = false }
+                                    }
+                                },
+                                onNavigateToAuth = {
+                                    navController.navigate(Destination.AuthScreen.route) {
+                                        popUpTo(Destination.ProfileScreen.route) { inclusive = true }
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+                        }
+                        composable(route = Destination.SettingsThemingScreen.route) {
+                            ThemingScreen(
+                                navController = navController,
+                                switchDynamicTheme = {
+                                    isDynamic.value = it
+                                    appSharedPrefsRepository.isDynamic = it
+                                },
+                            )
+                        }
+                        composable(route = Destination.SettingsStorageScreen.route) {
+                            StorageScreen(
+                                navController = navController,
+                            )
+                        }
+                    }
                 }
             }
         }
